a6322d5929d7dd9631fa968106cd44488fd99b0b
Jakub-Igntaowicz <kuba_ign@proton.me>
2023-11-21 23:38:45 +0100

    Added scan method and implemented whole main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::process;
 use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 struct Arguments {
     flag: String,
@@ -15,7 +21,6 @@ impl Arguments {
         } else if args.len() > 4 {
             return Err("too many arguments");
         }
-
         let f = args[1].clone();
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
             return Ok(Arguments {
@@ -32,7 +37,7 @@ impl Arguments {
                 );
                 return Err("help");
             } else if flag.contains("-h") || flag.contains("-help") {
-                return Err("Too may arguments for -h or -help flag");
+                return Err("too many arguments");
             } else if flag.contains("-j") {
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
@@ -40,7 +45,7 @@ impl Arguments {
                 };
                 let threads = match args[2].parse::<u16>() {
                     Ok(s) => s,
-                    Err(_) => return Err("failed to parser thread number"),
+                    Err(_) => return Err("failed to parse thread number"),
                 };
                 return Ok(Arguments {
                     threads,
@@ -54,4 +59,57 @@ impl Arguments {
     }
 }
 
-fn main() {}
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(0);
+        }
+    });
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
+}
